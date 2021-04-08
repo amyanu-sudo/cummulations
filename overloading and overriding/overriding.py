848dbb7683be7dc0e712e5cb330ff2e89fa315c3
amyanu-sudo <81663860+amyanu-sudo@users.noreply.github.com>
2021-04-08 17:40:06 +0530

    Create overriding.py
    In the child class Herbivorous we have overridden the method feed().
    So now when we create an object of the class Herbivorous and call the method feed() the overridden version will be executed.

---
+++ overloading and overriding/overriding.py
@@ -0,0 +1,14 @@
+class Animal:
+	def breathe(self):
+	    print("I breathe oxygen.")
+    
+	def feed(self):
+	    print("I eat food.")
+
+class Herbivorous(Animal):
+	def feed(self):
+	    print("I eat only plants. I am vegetarian.")
+      
+herbi = Herbivorous()
+herbi.feed()
+herbi.breathe()
