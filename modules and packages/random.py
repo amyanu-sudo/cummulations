910ab8b3ebbee3b797f8f157beecc549f57a8a37
amyanu-sudo <81663860+amyanu-sudo@users.noreply.github.com>
2021-04-08 18:45:36 +0530

    Create random.py
    Imports a randint and choice functions from random module.

---
+++ modules and packages/random.py
@@ -0,0 +1,7 @@
+from random import randint,choice
+random_integer = randint(1, 10)
+#prints the random integer between 1 and 10
+print(random_integer)
+random_ele = choice(["A","B","C"])
+#prints the random alphabet.
+print(random_ele)
