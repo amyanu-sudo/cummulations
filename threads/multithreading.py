b523c7350362fef88c87e69a9fd8b3b4a94c6d1c
amyanu-sudo <81663860+amyanu-sudo@users.noreply.github.com>
2021-04-12 17:03:20 +0530

    Create multithreading.py

---
+++ threads/multithreading.py
@@ -0,0 +1,22 @@
+import threading
+#function to print cube of given num
+def print_cube(num):
+    print("Cube: {}".format(num * num * num))
+  
+#function to print square of given num
+def print_square(num):
+    print("Square: {}".format(num * num))
+  
+# creating thread
+t1 = threading.Thread(target=print_square, args=(10,))
+t2 = threading.Thread(target=print_cube, args=(10,))
+# starting thread 1
+t1.start()
+# starting thread 2
+t2.start()
+# wait until thread 1 is completely executed
+t1.join()
+# wait until thread 2 is completely executed
+t2.join()
+# both threads completely executed
+print("Done!")
