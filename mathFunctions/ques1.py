a2350ee0aad30cee0f96168d93a8aecb33665193
amyanu-sudo <81663860+amyanu-sudo@users.noreply.github.com>
2021-04-12 18:54:37 +0530

    Create ques1.py

---
+++ mathFunctions/ques1.py
@@ -0,0 +1,9 @@
+import math
+#prints square root of a given number
+print(math.sqrt(64))
+#prints ceil of a given number
+print(math.ceil(1.4))
+#prints floor to the given number
+print(math.floor(1.4))
+#prints pi value
+print(math.pi)
